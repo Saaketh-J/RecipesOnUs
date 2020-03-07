63988f85c343ccfbb26f6a8165022a558fe56f59
Saaketh Jasti <saakethkjasti@outlook.com>
2020-03-07 11:29:31 -0600

    Ingredients Screen
    Finished the ingredients screen

--- recipes/lib/app_screens/ingredients_select.dart
+++ recipes/lib/app_screens/ingredients_select.dart
@@ -1,40 +1,52 @@
 import 'package:flutter/material.dart';
 
+List<String> ingredientsList = List(0);
+
 class ingredients extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomPadding: false,
         body: Container(
-          color: Color(0xff4A4C56),
-      alignment: Alignment.center,
-      child: Column(children: <Widget>[
-        Container(
-          margin:EdgeInsets.only(top: 40.0),
-          child: Text(
-            "Enter Ingredients: ",
-            style:TextStyle(fontSize: 40.0, color: Colors.white),
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.only(top: 30.0),
-        ),
-        Container(
-          child: TextBoxes(),
-        ),
-        Container(
-          child: TextBoxes(),
-        ),
-        Container(
-          child: TextBoxes(),
-        ),
-        Container(
-          child: TextBoxes(),
-        ),
-        Container(
-          child: TextBoxes(),
-        ),
-      ]),
-    ));
+          color: Colors.deepOrangeAccent,
+          alignment: Alignment.center,
+          child: ListView(children: <Widget>[
+            Center(
+              child: Container(
+                margin: EdgeInsets.only(top: 40.0),
+                child: Text(
+                  "Enter Main Ingredients: ",
+                  style: TextStyle(
+                      fontSize: 40.0,
+                      color: Color(0xff000080),
+                      fontFamily: 'CormorantGaramond',
+                      fontWeight: FontWeight.w500),
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 30.0),
+            ),
+            Container(
+              child: TextBoxes(),
+            ),
+            Container(
+              child: TextBoxes(),
+            ),
+            Container(
+              child: TextBoxes(),
+            ),
+            Container(
+              child: TextBoxes(),
+            ),
+            Container(
+              child: TextBoxes(),
+            ),
+            Container(
+              child: NextScreen(),
+            )
+          ]),
+        ));
   }
 }
 
@@ -52,23 +64,55 @@ class _TextBoxesState extends State<TextBoxes> {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
+        margin: EdgeInsets.only(bottom: 20.0, right: 18.0, left: 18.0),
         child: TextField(
+            style: TextStyle(
+              height: 2.0,
+              fontSize: 18.0,
+            ),
             decoration: InputDecoration(
                 hintText: 'Enter Ingredient',
                 filled: true,
                 fillColor: Colors.white,
+                enabledBorder: const OutlineInputBorder(
+                  borderSide: const BorderSide(
+                      color: Colors.deepOrangeAccent, width: 0.0),
+                ),
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(17.0),
-
-                )),
-
+                    //borderRadius: BorderRadius.circular(27.0),
+                    //borderSide: BorderSide(color: Colors.deepOrangeAccent, width: 0.0),
+                    )),
             onSubmitted: (var userInput) {
               setState(() {
                 textBox = userInput;
               });
+              ingredientsList.add(userInput);
             }),
-        padding: EdgeInsets.only(bottom: 20.0),
       ),
     );
+  }
+}
+
+class NextScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.only(top: 20.0, right: 70.0, left: 70.0),
+        height: 50.0,
+        child: RaisedButton(
+            color: Colors.deepOrange,
+            child: Text(
+              "Select Cuisine",
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+                fontFamily: 'Garamond',
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            elevation: 6.0,
+            onPressed: () {
+              //TODO
+            }));
   }
 }
--- recipes/lib/main.dart
+++ recipes/lib/main.dart
@@ -5,9 +5,36 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "Recipes On Us",
-    home: ingredients(),
+    home: MyApp(),
   ));
 }
 
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    new Future.delayed(
+        const Duration(seconds: 1),
+        () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ingredients()),
+            ));
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("images/chocolate.jpg"), fit: BoxFit.cover)),
+      ),
+    );
+  }
+}
